a91601d542914af1ebb6880d5127b4d145ae0935
Emistres <kristian.horner1@gmail.com>
2026-02-06 09:52:52 +0000

    Refactor BakeryClicker.py with improved structure

--- BakeryClicker.py
+++ BakeryClicker.py
@@ -8,6 +8,8 @@ import time
 
 class Player:
 
+    #dhirtsdjfnhisfhishhisdtsih
+
     # Will include all data on player
     # Try to setup to interact with save data
 
@@ -117,4 +119,5 @@ def main():
 
 
 if __name__ == "__main__":
+
     main()
